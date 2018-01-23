81e2ed9dff015f4cba70e74b5c887ce327e67a09
Sabine <matisuo@gmail.com>
2018-01-23 21:01:30 +0100

    commenting scrapping and send_email

--- scrapping.rb
+++ scrapping.rb
@@ -1,27 +1,27 @@
-require 'google_drive'
+require 'google_drive' #gem for accessing google sheet
 require 'rubygems'
-require 'nokogiri'
+require 'nokogiri'   #gems to open pages and get nodes
 require 'open-uri'
 
 
-def get_all_the_urls_of_seinr_saint_denis()
+def get_all_the_urls_of_seine_saint_denis() #methode that gets all the town's url of the 93's page and put in the sheet town name and email
 
-	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/seine-saint-denis.html"))
-	cities = ""
-	page.css('a.lientxt').each do |town|
-		site = "https://annuaire-des-mairies.com" + town['href'][1..-1]
-		cities = town.text
-		$ws[$i, 1] = cities
-		$ws[$i, 2] = get_the_email_of_a_townhal_from_its_webpage(site)
-		$i += 1
-		puts "writting :  #{cities} => #{get_the_email_of_a_townhal_from_its_webpage(site)}"
+	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/seine-saint-denis.html")) #opening page of all cities of 93
+	cities = "" #initializing 
+	page.css('a.lientxt').each do |town| #for each <a> with a class "lientxt" ...
+		site = "https://annuaire-des-mairies.com" + town['href'][1..-1] # the variable site starts with "htt...." and add the href and from it removing the . before the /
+		cities = town.text #take the content of <a> </a> and turn it into a str
+		$ws[$i, 1] = cities #in the cell line i (counter) column 1 write what in the variable cities (alias the town name)
+		$ws[$i, 2] = get_the_email_of_a_townhal_from_its_webpage(site) #in line i column 2 write the email by calling the get email methode with the rebuild site as argument
+		$i += 1 #add 1 to  the counter so it goes to the next line in the sheet
+		puts "writting :  #{cities} => #{get_the_email_of_a_townhal_from_its_webpage(site)}" #displays what city and what email its taking care of
 	
 	end
 
 
 end
 
-def get_all_the_urls_of_val_doise_townhalls()
+def get_all_the_urls_of_val_doise_townhalls()#does quite the same as the methode above
 	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 	
 	cities = ""
@@ -37,25 +37,25 @@ def get_all_the_urls_of_val_doise_townhalls()
 end
 
 
-def get_the_email_of_a_townhal_from_its_webpage (adresse)
-	page = Nokogiri::HTML(open(adresse))  
+def get_the_email_of_a_townhal_from_its_webpage (adresse) #methode that use the "rebuilt" link as argument to get the email of a town
+	page = Nokogiri::HTML(open(adresse))  #opens the page of the link given in argument 
 	
-	return page.css('td.style27 p.Style22 font')[6].text
+	return page.css('td.style27 p.Style22 font')[6].text #returns the 6th value found => email into str
 end
 
-def get_all_data_to_google()
+def get_all_data_to_google()  #starting method
 
-	session = GoogleDrive::Session.from_config("config.json")
-	$ws = session.spreadsheet_by_key("1M1vJ2XhdkrV2JvmHb5RauwYmHlHWv1k-AbdXTFx7Ti8").worksheets[0]
+	session = GoogleDrive::Session.from_config("config.json") #initialize a googleDrive session via the config.json file
+	$ws = session.spreadsheet_by_key("1M1vJ2XhdkrV2JvmHb5RauwYmHlHWv1k-AbdXTFx7Ti8").worksheets[0] #put into a cross funtion variable the  1st page of the google sheet given
 
 
-	$i = 1
-	get_all_the_urls_of_seinr_saint_denis()
-	get_all_the_urls_of_val_doise_townhalls()
-	$ws.save
-	puts "saved!"
+	$i = 1 #initialize counter
+	get_all_the_urls_of_seine_saint_denis() #fetch the first area's email addresses
+	get_all_the_urls_of_val_doise_townhalls()#fetch the second area's email addresses
+	$ws.save #save what's been previously written
+	puts "saved!" #tells us that it saved
 
 end
 
 
-get_all_data_to_google()
+get_all_data_to_google() #calls the starting function
--- send_emails.rb
+++ send_emails.rb
@@ -1,9 +1,9 @@
-require 'gmail'
-require 'google_drive'
-require 'rubygems'
+require 'gmail'       #gem gmail
+require 'google_drive' #gem to get on google sheet
+require 'rubygems'      
 require 'io/console'   #required so you can type your password in a blind field
 
-def get_the_email_html(town)
+def get_the_email_html(town)  #this methode returns a string of a html code with the name of the town given in argument in it
 
 	return "<h1>Bonjour</h1>
 					<p>Je m'appelle Mati, je suis élève à une formation de code gratuite, ouverte à tous, sans restriction géographique, ni restriction de niveau.<br> La formation s'appelle The Hacking Project (http://thehackingproject.org/).<br> Nous apprenons l'informatique via la méthode du peer-learning : nous faisons des projets concrets qui nous sont assignés tous les jours (par exemple envoyer des mails via ruby), sur lesquel nous planchons en petites équipes autonomes. Le projet du jour est d'envoyer des emails à nos élus locaux pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation gratuite.</p>
@@ -15,60 +15,58 @@ def get_the_email_html(town)
 end
 
 
-def send_email_to_line(dest, town)
+def send_email_to_line(dest, town) #methode that uses gmail gem to send an email to the destination in argument and gives to the html methode the name of the town given in arguments too
 
-	gmail = Gmail.connect($email, $pass)
+	gmail = Gmail.connect($email, $pass) #connection line uses a cross function variable ($)
 
-		if gmail.logged_in? then puts "connected" else puts "offline" end
+		if gmail.logged_in? then puts "connected" else puts "offline" end # tells us if the login worked
 		
-		gmail.deliver do
-			puts "preparing email for #{town} to #{dest}"
-		  to dest
-  		subject "Formation : The Hacking Project"
-  		html_part do
-  			content_type 'text/html; charset=UTF-8'
-  			body get_the_email_html(town)  			
+		gmail.deliver do #generating email
+			puts "preparing email for #{town} to #{dest}" #warn us that we entered the function that will send a mail to the town in arg at the email in arg
+		  to dest #gets back the destination from the arg
+  		subject "Formation : The Hacking Project" #subject of the mail
+  		html_part do # function used to write a mail in html code
+  			content_type 'text/html; charset=UTF-8' #encoding parametters
+  			body get_the_email_html(town)  			#calling the content of the mail from the methode above and giving it the name of the town so it can modify it in the html
   		end
   	end
 
-  puts "sent"
-	gmail.logout
+  puts "sent" #when done prints a sent
+	gmail.logout #disconnect from email address
 end
 
-def go_through_all_the_lines()
-	g_session = GoogleDrive::Session.from_config("config.json")
-	w_sheet = g_session.spreadsheet_by_key("1M1vJ2XhdkrV2JvmHb5RauwYmHlHWv1k-AbdXTFx7Ti8").worksheets[0]
+def go_through_all_the_lines()  #methode to read all the lines of the google sheet
+	g_session = GoogleDrive::Session.from_config("config.json") #starting drive session from the config.json 's keys'
+	w_sheet = g_session.spreadsheet_by_key("1M1vJ2XhdkrV2JvmHb5RauwYmHlHWv1k-AbdXTFx7Ti8").worksheets[0] #asking for the first page of the google sheet at that link
 
-	(1..w_sheet.num_rows).each do |x|
+	(1..w_sheet.num_rows).each do |x| #for each lines of the sheet ...
 
-		mail_addresse = w_sheet[x, 2]
-		city_name = w_sheet[x, 1]
-		puts "\n---------------------------------"
-		puts "sending mail to : #{city_name}"
-		if mail_addresse.include?("@") then send_email_to_line(mail_addresse, city_name) 
-		else puts "#{city_name} has given us no email address" end
+		mail_addresse = w_sheet[x, 2]  #the mail is in the 2nd column
+		city_name = w_sheet[x, 1]      #the name is in the 1st column
+		puts "\n---------------------------------" # bar to split the differents notification at every loop ... cleaner on the console
+		puts "sending mail to : #{city_name}"      # warning us that it will send a mail to current city
+		if mail_addresse.include?("@") then send_email_to_line(mail_addresse, city_name) #if the cell where the email is supposed to be has a email adress it goes to the email methode
+		else puts "#{city_name} has given us no email address" end #else if it doesn't find any "@" it the cell means there is no addresses so it return a message saying there is no email
 
 	end
 
 end
 
-def lets_make_this_work()
+def lets_make_this_work() #initializing methode takes no argument
 
-	puts "Welcome to my TownHall spam program ;)"
-	puts "--------------------------------------\n \n"
+	puts "Welcome to my TownHall spam program ;)" #welcoming line
+	puts "--------------------------------------\n \n"#splitting line
 
-	puts "Please enter your email address :"
-	$email = gets.chomp
-	puts "Password ? (this is a blind field)"
-	$pass = STDIN.noecho(&:gets).chomp
+	puts "Please enter your email address :"  #asks user to write its email adress
+	$email = gets.chomp #saves what the user enter in to a cross function variable
+	puts "Password ? (this is a blind field)" #askes user to write its password
+	$pass = STDIN.noecho(&:gets).chomp #uses a blind field to type safely  the email's password (refer to require'io/console') and get the password into a cross functions variable
 
 
-	go_through_all_the_lines()
+	go_through_all_the_lines()  #gets to the methode that starts reading the sheet
 
 end
 
 
-lets_make_this_work()
-#225mails
-#
+lets_make_this_work() #starts the program
 
